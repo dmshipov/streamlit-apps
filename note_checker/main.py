f06b994b212586f8cc66b060c7a3d820eb64d08b
shipov_dmitriy <dmshipov@bk.ru>
2024-11-01 01:13:10 +0300

    new_options

--- note_checker/main.py
+++ note_checker/main.py
@@ -1,7 +1,6 @@
 
 import streamlit as st
 import pandas as pd
-import time
 import datetime
 import sqlite3
 
@@ -17,18 +16,16 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         Товар TEXT,
-        Значение REAL,
-        Количество INTEGER
+        Значение INTEGER,
+        Количество INTEGER,
+        Вес INTEGER
+               
     )
 ''')
 conn.commit()
 
 # Загрузка данных из базы данных
 products = pd.read_sql_query("SELECT * FROM products", conn)
-
-# Инициализация сессии
-if 'initial_text' not in st.session_state:
-    st.session_state.initial_text = ""
 
 # Функция для очистки текста и списка продуктов
 def clear_text():
@@ -51,8 +48,6 @@ def clear_text():
     conn.close()
 
 def update_text():
-    # Задержка
-    time.sleep(0.5)
 
     # Разделение текста
     lines = st.session_state.text_input.split(' и ')
@@ -65,17 +60,18 @@ def update_text():
     for line in lines:
         parts = line.split(' И ')
         for part in parts:
-            products_list.append({"Товар": part.strip(), "Значение": 0, "Количество": 1})
+            products_list.append({"Товар": part.strip(), "Значение": 0, "Количество": 1, "Вес": 0})
 
     # Сохранение данных в базу данных
     for product in products_list:
-        cursor.execute("INSERT INTO products (Товар, Значение, Количество) VALUES (?, ?, ?)",
-                        (product["Товар"], product["Значение"], product["Количество"]))
+        cursor.execute("INSERT INTO products (Товар, Значение, Количество, Вес) VALUES (?, ?, ?, ?)",
+                        (product["Товар"], product["Значение"], product["Количество"], product['Вес']))
         conn.commit()
 
     # Обновление DataFrame
     products = pd.read_sql_query("SELECT * FROM products", conn)
     st.session_state.products = products.copy()
+    st.selectbox("Выберите опцию", ["Без расчета", "C расчетом", "C расчетом и весом"])
 
 # Создаем форму
 form = st.form("Моя форма")
@@ -86,14 +82,13 @@ form.text_area("Введите текст", key='text_input')
 # Кнопка для преобразования в таблицу
 if form.form_submit_button("Преобразовать в таблицу"):
     update_text()
-    st.session_state.initial_text = ""
     st.rerun()
     
 
 # Отрисовка таблицы только если текст не пуст
 if not products.empty:
     # Элементы управления для сортировки в боковой панели
-    sort_by = st.sidebar.selectbox("Сортировать по:", ["Товар", "Значение", "Количество"])
+    sort_by = st.sidebar.selectbox("Сортировать по:", ["Товар", "Значение", "Количество", "Вес"])
     sort_order = st.sidebar.radio("Порядок сортировки:", ["По убыванию", "По возрастанию"])
 
     # Применяем сортировку
@@ -105,11 +100,11 @@ if not products.empty:
     selected_indices = []  # Список для хранения выбранных индексов
 
     # Выбор опции один раз для всех товаров
-    option = st.sidebar.selectbox("Выберите опцию", ["Без расчета", "Добавить расчет"])
+    option = st.selectbox("Выберите опцию", ["Без расчета", "C расчетом суммы", "C расчетом количества", "C расчетом веса", "Все расчеты"])
 
     # Создаем таблицу для ввода цены и количества
     for index, row in sorted_products.iterrows():
-        col1, col2, col3 = st.columns([2, 0.4, 0.55])  # Создаем три столбца
+        col1, col2, col3, col4, col5 = st.columns([2, 0.5, 0.7, 0.5, 0.4])  # Создаем четыре столбца
 
         with col1:
             st.markdown("<br>", unsafe_allow_html=True)
@@ -118,7 +113,7 @@ if not products.empty:
                 selected_indices.append(index)  # Добавляем индекс в список выбранных
 
         with col2:
-            if option == "Добавить расчет":  # Проверяем выбранную опцию
+            if option == "C расчетом суммы" or option == "Все расчеты":  # Проверяем выбранную опцию
                 # Сохраняем новое значение в session_state
                 if f'price_{index}' not in st.session_state:
                     st.session_state[f'price_{index}'] = str(row['Значение'])  
@@ -143,13 +138,13 @@ if not products.empty:
 
 
         with col3:
-            if option == "Добавить расчет":  # Проверяем выбранную опцию
+            if option == "C расчетом количества" or option == "Все расчеты":  # Проверяем выбранную опцию
                 # Ввод количества с преобразованием в int
                 quantity = st.number_input("Количество", 
                                             min_value=0,
                                             format="%d",
                                             key=f'quantity_{index}', 
-                                            value=row['Количество'])
+                                            value=st.session_state.get(f'quantity_{index}', row['Количество']))
 
                 # Преобразуем в int только если введено значение
                 if quantity:
@@ -161,16 +156,52 @@ if not products.empty:
                     except ValueError:
                         st.error("Введите корректное значение для 'Количество'")
                         products.at[index, "Количество"] = None  # Или оставьте None
+               # Чекбокс для удаления строки
+        with col5:
+            st.markdown("<br>", unsafe_allow_html=True)
+            delete_checkbox = st.button("del", key=f'delete_{index}')
+            if delete_checkbox:
+                # Удаляем строку из DataFrame
+                products.drop(index, inplace=True)
+                # Удаляем запись из базы данных
+                cursor.execute("DELETE FROM products WHERE id=?", (row['id'],))
+                conn.commit()
+                # Обновляем данные в st.session_state
+                st.session_state.products = products
+                # Перезагружаем компонент DataFrame
+                st.rerun()
+
+        with col4:
+            if option == "C расчетом веса" or option == "Все расчеты":  # Проверяем выбранную опцию
+                # Ввод веса с преобразованием в int
+                weight = st.number_input("Вес в гр.", 
+                                        min_value=0,
+                                        format="%d",
+                                        key=f'weight_{index}', 
+                                        value=st.session_state.get(f'weight_{index}', row['Вес']))
+
+                # Преобразуем в int только если введено значение
+                if weight:
+                    try:
+                        products.at[index, "Вес"] = int(weight)
+                        # Обновляем вес в базе данных (если есть столбец "Вес")
+                        cursor.execute("UPDATE products SET Вес=? WHERE id=?", (int(weight), row['id']))
+                        conn.commit()
+                    except ValueError:
+                        st.error("Введите корректное значение для 'Вес'")
+                        products.at[index, "Вес"] = None  # Или оставьте None
+
 
     # Вычисляем общую сумму и количество для выбранных товаров
     if selected_indices:
         total_sum = (products.loc[selected_indices, "Значение"] * 
                      products.loc[selected_indices, "Количество"]).sum()
         total_quantity = products.loc[selected_indices, "Количество"].sum()
+        total_weight = products.loc[selected_indices, "Вес"].sum()  # Сумма веса
         
         st.write(f"Общая сумма: {total_sum:.2f}")
         st.write(f"Общее количество: {int(total_quantity)}")
-
+        st.write(f"Общий вес: {total_weight} грамм")  # Вывод общей суммы веса
         # Кнопка для скачивания таблицы в формате Excel
         excel_file_path = "products.xlsx"
 
