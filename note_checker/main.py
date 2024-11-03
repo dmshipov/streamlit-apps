ad1891e1ea297ed6779ecf7f1ef900fbc4e88975
Dmitriy_Shipov <95577938+dmshipov@users.noreply.github.com>
2024-11-04 02:47:54 +0300

    Update main.py

--- note_checker/main.py
+++ note_checker/main.py
@@ -2,11 +2,8 @@ import streamlit as st
 import pandas as pd
 import datetime
 import sqlite3
-import uuid
-from io import BytesIO
 from PIL import Image
 
-key = str(uuid.uuid4())
 # Создаем соединение с базой данных
 conn = sqlite3.connect('my_data.db')
 cursor = conn.cursor()
@@ -25,11 +22,11 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT,
-        Товар TEXT,
+        Наименование TEXT,
         Значение INTEGER,
         Количество INTEGER,
         Вес INTEGER,
-        Изображение BLOB
+        Фото BLOB
     )
 ''')
 
@@ -103,11 +100,11 @@ else:
         for line in lines:
             parts = line.split(' И ')
             for part in parts:
-                products_list.append({"Товар": part.strip(), "Значение": 0, "Количество": 1, "Вес": 0, 'Изображение': None})
+                products_list.append({"Наименование": part.strip(), "Значение": 0, "Количество": 1, "Вес": 0, 'Фото': None})
 
         for product in products_list:
-            cursor.execute("INSERT INTO products (username, Товар, Значение, Количество, Вес, Изображение) VALUES (?, ?, ?, ?, ?, ?)",
-                           (st.session_state.username, product["Товар"], product["Значение"], product["Количество"], product['Вес'], product['Изображение']))
+            cursor.execute("INSERT INTO products (username, Наименование, Значение, Количество, Вес, Фото) VALUES (?, ?, ?, ?, ?, ?)",
+                           (st.session_state.username, product["Наименование"], product["Значение"], product["Количество"], product['Вес'], product['Фото']))
         conn.commit()
 
         products = pd.read_sql_query("SELECT * FROM products WHERE username=?", conn, params=(st.session_state.username,))
@@ -128,19 +125,19 @@ else:
     # Отрисовка таблицы только если текст не пуст
     if not products.empty:
         # Элементы управления для сортировки в боковой панели
-        sort_by = st.sidebar.selectbox("Сортировать по:", ["Товар", "Значение", "Количество", "Вес"])
+        sort_by = st.sidebar.selectbox("Сортировать по:", ["Наименование", "Значение", "Количество", "Вес"])
         sort_order = st.sidebar.radio("Порядок сортировки:", ["По убыванию", "По возрастанию"])
 
         # Применяем сортировку
-        if sort_by == "Товар":
+        if sort_by == "Наименование":
             sorted_products = products.copy()
         else:
             sorted_products = products.sort_values(by=sort_by, ascending=(sort_order == "По возрастанию"))
 
         selected_indices = []  # Список для хранения выбранных индексов
 
-        # Выбор опции один раз для всех товаров
-        option = st.selectbox("Выберите опцию", ["Без расчета", "C расчетом суммы", "C расчетом количества", "C расчетом веса", "C изображением", "Все расчеты"])
+        # Выбор опции один раз для всех Наименованиеов
+        option = st.selectbox("Выберите опцию", ["   ", "Значение", "Количество", "Вес", "Фото", "Все"])
 
         # Создаем таблицу для ввода цены и количества
         for index, row in sorted_products.iterrows():
@@ -148,12 +145,12 @@ else:
 
             with col1:
                 st.markdown("<br>", unsafe_allow_html=True)
-                checkbox = st.checkbox(f"{row['Товар']}", key=f'checkbox_{index}')  # Чекбокс для выбора товара
+                checkbox = st.checkbox(f"{row['Наименование']}", key=f'checkbox_{index}')  # Чекбокс для выбора Наименованиеа
                 if checkbox:
                     selected_indices.append(index)  # Добавляем индекс в список выбранных
 
             with col2:
-                if option == "C расчетом суммы" or option == "Все расчеты":  # Проверяем выбранную опцию
+                if option == "Значение" or option == "Все":  # Проверяем выбранную опцию
                     # Сохраняем новое значение в session_state
                     if f'price_{index}' not in st.session_state:
                         st.session_state[f'price_{index}'] = str(row['Значение'])  
@@ -178,7 +175,7 @@ else:
 
 
             with col3:
-                if option == "C расчетом количества" or option == "Все расчеты":  # Проверяем выбранную опцию
+                if option == "Количество" or option == "Все":  # Проверяем выбранную опцию
                     # Ввод количества с преобразованием в int
                     quantity = st.number_input("Количество", 
                                                 min_value=0,
@@ -198,7 +195,7 @@ else:
                             products.at[index, "Количество"] = None  # Или оставьте None
                 
             with col4:
-                if option == "C расчетом веса" or option == "Все расчеты":  # Проверяем выбранную опцию
+                if option == "Вес" or option == "Все":  # Проверяем выбранную опцию
                     # Ввод веса с преобразованием в int
                     weight = st.number_input("Вес в гр.", 
                                             min_value=0,
@@ -217,15 +214,29 @@ else:
                             st.error("Введите корректное значение для 'Вес'")
                             products.at[index, "Вес"] = None  # Или оставьте None
             with col5:  # Новый столбец для загрузки фото с камеры
-                if option == "C изображением" or option == "Все расчеты":
-                    image_file = st.camera_input("Фото", key=f'image_{index}')
-                    if image_file is not None:
-                        # Сохраняем изображение в базу данных (предполагается, что вы используете BLOB)
-                        image_bytes = image_file.read()
-                        products.at[index, "Изображение"] = image_bytes
+                if option == "Фото" or option == "Все расчеты":
+                    if products.at[index, "Фото"] is not None:
+                        # Если фото в базе данных, отображаем его
+                        st.image(products.at[index, "Фото"], caption='Фото', use_column_width=True)
                         
-                        cursor.execute("UPDATE products SET Изображение=? WHERE id=?", (image_bytes, row['id']))
-                        conn.commit()
+                        # Добавляем кнопку "Удалить фото"
+                        if st.button("Удалить фото", key=f"delete_photo_{index}"):
+                            products.at[index, "Фото"] = None
+                            cursor.execute("UPDATE products SET Фото=? WHERE id=?", (None, row['id']))
+                            conn.commit()
+                            st.rerun()
+                    else:
+                        # Если фото нет, показываем кнопку "Загрузить фото"
+                        image_file = st.camera_input("Фото", key=f'image_{index}')
+                        if image_file is not None:  # Проверка внутри блока if
+                            # Сохраняем изображение в базу данных
+                            image_bytes = image_file.read()
+                            products.at[index, "Фото"] = image_bytes
+                            
+                            cursor.execute("UPDATE products SET Фото=? WHERE id=?", (image_bytes, row['id']))
+                            conn.commit()
+                    
+                    
             with col6:
                 st.markdown("<br>", unsafe_allow_html=True)
                 delete_checkbox = st.button("Удалить позицию", key=f'delete_{index}')
@@ -240,14 +251,14 @@ else:
                     # Перезагружаем компонент DataFrame
                     st.rerun()
 
-        # Вычисляем общую сумму и количество для выбранных товаров
+        # Вычисляем общую сумму и количество для выбранных Наименованиеов
         if selected_indices:
             total_sum = (products.loc[selected_indices, "Значение"] * 
                         products.loc[selected_indices, "Количество"]).sum()
             total_quantity = products.loc[selected_indices, "Количество"].sum()
             total_weight = products.loc[selected_indices, "Вес"].sum()  # Сумма веса
             
-            st.write(f"Общая сумма: {total_sum:.2f}")
+            st.write(f"Общая сумма значений: {total_sum:.2f}")
             st.write(f"Общее количество: {int(total_quantity)}")
             st.write(f"Общий вес: {total_weight} грамм")  # Вывод общей суммы веса
 
@@ -261,7 +272,7 @@ else:
         excel_file_path = f"{st.session_state.username}.xlsx"
         # Используем openpyxl вместо xlsxwriter
         with pd.ExcelWriter(excel_file_path, engine='openpyxl') as writer:
-            products.to_excel(writer, index=False, sheet_name='Products')
+            products[["Наименование", "Значение", "Количество", "Вес"]].to_excel(writer, index=False, sheet_name='Products')
 
         with open(excel_file_path, "rb") as f:
             # Получаем текущую дату и время в формате "YYYY-MM-DD_HH-MM-SS"
