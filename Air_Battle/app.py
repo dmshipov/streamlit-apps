183d0c0d69a4fd50f190f7de832fdf92776db238
Dmitriy <dmshipov@bk.ru>
2026-02-05 23:48:04 +0300

    new_changes

--- Air_Battle/app.py
+++ Air_Battle/app.py
@@ -591,9 +591,10 @@ game_html = """
         try {
             peer = new Peer({
                 config: {
-                    iceServers: [
+                    'iceServers': [
                         { urls: 'stun:stun.l.google.com:19302' },
-                        { urls: 'stun:global.stun.twilio.com:3478' }
+                        { urls: 'stun:stun1.l.google.com:19302' },
+                        { urls: 'stun:stun2.l.google.com:19302' }
                     ]
                 }
             });
