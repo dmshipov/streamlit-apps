230e6ccf0da652a776da369a4e47bac2711401a5
Dmitriy <dmshipov@bk.ru>
2026-02-06 00:16:35 +0300

    new_changes

--- Air_Battle/app.py
+++ Air_Battle/app.py
@@ -590,11 +590,13 @@ game_html = """
     function initPeer() {
         try {
             peer = new Peer({
+                host: '0.peerjs.com',
+                port: 443,
+                secure: true,
                 config: {
                     'iceServers': [
                         { urls: 'stun:stun.l.google.com:19302' },
-                        { urls: 'stun:stun1.l.google.com:19302' },
-                        { urls: 'stun:stun2.l.google.com:19302' }
+                        { urls: 'stun:stun1.l.google.com:19302' }
                     ]
                 }
             });
